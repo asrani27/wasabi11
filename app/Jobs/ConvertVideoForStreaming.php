eee0f1b875f1e717d1d8718f54a1d5cc0dbfd049
asrani <asrani.27@gmail.com>
2024-09-16 12:35:09 +0800

    f

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -5,10 +5,12 @@ namespace App\Jobs;
 use App\Models\Upload;
 use FFMpeg\Format\Video\X264;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use ProtoneMedia\LaravelFFMpeg\FFMpeg\FFProbe;
 use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class ConvertVideoForStreaming implements ShouldQueue
@@ -30,6 +32,8 @@ class ConvertVideoForStreaming implements ShouldQueue
         $lowBitrateFormat  = (new X264)->setKiloBitrate(480);
         $midBitrateFormat  = (new X264)->setKiloBitrate(720);
         $highBitrateFormat = (new X264)->setKiloBitrate(1080);
+
+        //$this->info('Converting sample.mp4');
 
         FFMpeg::fromDisk('public')
             ->open('mp4/SampleVideo_1280x720_30mb_5a4c772c3c799d932a0fe92f71a1e561.mp4')
