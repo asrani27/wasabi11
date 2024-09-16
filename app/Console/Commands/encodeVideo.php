e4fef01d330fb96d358f1291c1334502216c20ab
asrani <asrani.27@gmail.com>
2024-09-16 22:23:26 +0800

    f

--- app/Console/Commands/encodeVideo.php
+++ app/Console/Commands/encodeVideo.php
@@ -5,8 +5,10 @@ namespace App\Console\Commands;
 use FFMpeg\FFMpeg;
 use FFMpeg\Format\Video\X264;
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
 use Illuminate\Support\Facades\Storage;
 use ProtoneMedia\LaravelFFMpeg\FFMpeg\FFProbe;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class encodeVideo extends Command
 {
@@ -30,13 +32,44 @@ class encodeVideo extends Command
     public function handle()
     {
 
-        // $midBitrateFormat  = (new X264)->setKiloBitrate(720);
-        $ffmpeg = FFMpeg::create();
-        $video = $ffmpeg->open('http://vplayer.veenix.online/storage/mkv/Q.O.T.S01E10.720p.WEB-DL.H264-SEC_a4c01ebbf343076d3b5209292dcf197f.mkv');
+        $this->info('Converting mkv to hls');
 
+        $inputPath = 'http://vplayer.veenix.online/storage/mkv/Q.O.T.S01E10.720p.WEB-DL.H264-SEC_a4c01ebbf343076d3b5209292dcf197f.mkv';
         $outputPath = public_path() . '/stream/sample/sample.m3u8';
-        $this->info('Converting sample.mkv');
-        $video->hls()->save($outputPath);
+
+        $process = new Process([
+            'ffmpeg',
+            '-i',
+            $inputPath,
+            '-codec:',
+            'copy',
+            '-start_number',
+            '0',
+            '-hls_time',
+            '10',
+            '-hls_list_size',
+            '0',
+            '-f',
+            'hls',
+            $outputPath
+        ]);
+        // Jalankan perintah
+        $process->run();
+
+        // Periksa apakah proses gagal
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return response()->json(['success' => 'Video converted to HLS successfully']);
+        // return response()->json(['success' => 'Video converted to HLS successfully']);
+        // // $midBitrateFormat  = (new X264)->setKiloBitrate(720);
+        // $ffmpeg = FFMpeg::create();
+        // $video = $ffmpeg->open('http://vplayer.veenix.online/storage/mkv/Q.O.T.S01E10.720p.WEB-DL.H264-SEC_a4c01ebbf343076d3b5209292dcf197f.mkv');
+
+        // $outputPath = public_path() . '/stream/sample/sample.m3u8';
+        // $this->info('Converting sample.mkv');
+        // $video->hls()->save($outputPath);
 
         // FFMpeg::fromDisk('public')
         //     ->open('storage/mkv/Q.O.T.S01E10.720p.WEB-DL.H264-SEC_a4c01ebbf343076d3b5209292dcf197f.mkv')
