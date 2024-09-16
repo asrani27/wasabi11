54440c6e6bbd2f284dfaedb7d4724ed05981d6f6
asrani <asrani.27@gmail.com>
2024-09-16 09:17:17 +0800

    f

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use ProtoneMedia\LaravelFFMpeg\FFMpeg\FFProbe;
 use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class ConvertVideoForStreaming implements ShouldQueue
@@ -29,17 +30,16 @@ class ConvertVideoForStreaming implements ShouldQueue
     public function handle(): void
     {
 
-        
-        $lowBitrateFormat  = (new X264)->setKiloBitrate(480);
-        $midBitrateFormat  = (new X264)->setKiloBitrate(720);
-        $highBitrateFormat = (new X264)->setKiloBitrate(1080);
+        $ffprobe = FFProbe::create();
+        $video = $ffprobe->streams('https://vplayer.veenix.online/storage/'.$this->video->type . '/' . $this->video->filename)->videos()->first();
+        $res = $video->get('height');
+
+        $BitrateFormat  = (new X264)->setKiloBitrate($res);
 
         FFMpeg::fromDisk('videos')
             ->open($this->video->type . '/' . $this->video->filename)
             ->exportForHLS()
-            ->addFormat($lowBitrateFormat)
-            ->addFormat($midBitrateFormat)
-            ->addFormat($highBitrateFormat)
+            ->addFormat($BitrateFormat)
             ->toDisk('videos')
             ->save('stream/' . $this->video->short_file . '/' . $this->video->short_file . '.m3u8');
     }
