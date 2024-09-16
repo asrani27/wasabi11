8af28a09376eb56e611e122085e109fe7deded2a
asrani <asrani.27@gmail.com>
2024-09-16 12:40:42 +0800

    f

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -29,19 +29,13 @@ class ConvertVideoForStreaming implements ShouldQueue
      */
     public function handle(): void
     {
-        $lowBitrateFormat  = (new X264)->setKiloBitrate(480);
-        $midBitrateFormat  = (new X264)->setKiloBitrate(720);
-        $highBitrateFormat = (new X264)->setKiloBitrate(1080);
+        $BitrateFormat  = (new X264)->setKiloBitrate($this->video->resolusi);
 
-        //$this->info('Converting sample.mp4');
-
-        FFMpeg::fromDisk('public')
-            ->open('mp4/SampleVideo_1280x720_30mb_5a4c772c3c799d932a0fe92f71a1e561.mp4')
+        FFMpeg::fromDisk('videos')
+            ->open($this->video->type . '/' . $this->video->filename)
             ->exportForHLS()
-            ->addFormat($lowBitrateFormat)
-            ->addFormat($midBitrateFormat)
-            ->addFormat($highBitrateFormat)
+            ->addFormat($BitrateFormat)
             ->toDisk('videos')
-            ->save('stream/sample/sample.m3u8');
+            ->save('stream/' . $this->video->short_file . '/' . $this->video->short_file . '.m3u8');
     }
 }
