2a0f73eef926e2ec2e6ea1d2e11e6f57324e7fcb
asrani <asrani.27@gmail.com>
2024-09-18 20:05:53 +0800

    f

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -26,7 +26,7 @@
         @if ($data->type == 'mp4')
         <div class="card-footer">
             <strong>Embed Code :</strong>
-            <textarea class="form-control" disabled rows="4"><iframe height="360" width="640" frameBorder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="/stream/{{$data->short_file}}"></iframe>
+            <textarea class="form-control" disabled rows="4"><iframe height="360" width="640" frameBorder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="https://vplayer.veenix.online/stream/{{$data->short_file}}"></iframe>
             </textarea>
         </div>
         @endif
