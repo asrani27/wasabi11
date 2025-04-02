3190fa61a8fe43657c67e951dad8f486fad146bd
asrani <asrani.27@gmail.com>
2025-04-03 02:54:36 +0800

    f

--- resources/views/user/stream.blade.php
+++ resources/views/user/stream.blade.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8" />
   <meta name="robots" content="noindex" />
   <META NAME="GOOGLEBOT" CONTENT="NOINDEX" />
-  <title>Player Vidio</title>
+  <title>Veenix Player</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   <link href="https://assets.mediadelivery.net/rubik.css" rel="stylesheet">
   <script src="https://assets.mediadelivery.net/plyr/3.7.3.2/plyr.polyfilled.min.js"></script>
@@ -405,29 +405,7 @@
     </video>
   </div>
 
-
   <script>
-    var thumbnail_width = 300;
-
-    var seek_thumbnail_config = {
-        enabled: false,
-        pic_num: 10,// total thumbnail numbers
-        width: thumbnail_width,// per thumbnail item width
-        height: thumbnail_width * 9 / 16, // default height
-        col: 6,// per thumbnail image columns
-        row: 6,// per thumbnail image rows
-        offsetX: 0,
-        offsetY: 0,
-        urls: [
-            '',
-        ]
-    }
-
-    function getSeekThumbnailHeight(imgWidth, imgHeight) {
-        var aspectRatio = imgWidth / imgHeight;
-        return Math.floor(thumbnail_width / aspectRatio);
-    }
-
     function IsMobile() {
         var check = false;
         (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
@@ -438,7 +416,7 @@
         var video = document.querySelector("video");
         var loadingSpinner = document.getElementById("loading-spinner");
         var fragmentsLoaded = 0;
-        var player = null;
+        var videoplayer = null;
         var isFairplay = false;
         var useFairplayInternalEngine= false;
         
@@ -449,15 +427,14 @@
         var isAdPaused = false;
         var isAirplayEnabled = false;
 
-
         var video = document.getElementById("main-video");
+
         var loadingSpinner = document.getElementById("loading-spinner");
 
-            video.addEventListener("loadeddata", function () {
-                loadingSpinner.classList.add("hidden");
-            });
+        video.addEventListener("loadeddata", function () {
+            loadingSpinner.classList.add("hidden");
+        });
             
-
         video.addEventListener("click", function () {
             enterFullscreen();
         });
@@ -509,43 +486,7 @@
             ],
             settings: ['quality', 'speed', 'loop'],
             speed: { selected: 1, options: [0.5,0.75,1,1.25,1.5,1.75,2,4] },
-            i18n: {
-                restart: 'Restart',
-                rewind: 'Rewind {seektime}s',
-                play: 'Play',
-                pause: 'Pause',
-                fastForward: 'Forward {seektime}s',
-                seek: 'Seek',
-                seekLabel: '{currentTime} of {duration}',
-                played: 'Played',
-                buffered: 'Buffered',
-                currentTime: 'Current time',
-                duration: 'Duration',
-                volume: 'Volume',
-                mute: 'Mute',
-                unmute: 'Unmute',
-                enableCaptions: 'Enable captions',
-                disableCaptions: 'Disable captions',
-                download: 'Download',
-                enterFullscreen: 'Enter fullscreen',
-                exitFullscreen: 'Exit fullscreen',
-                frameTitle: 'Player for {title}',
-                captions: 'Captions',
-                settings: 'Settings',
-                pip: 'PIP',
-                menuBack: 'Go back to previous menu',
-                speed: 'Speed',
-                normal: 'Normal',
-                quality: 'Quality',
-                loop: 'Loop',
-                start: 'Start',
-                end: 'End',
-                all: 'All',
-                reset: 'Reset',
-                disabled: 'Disabled',
-                enabled: 'Enabled',
-            },
-            thumbnail: seek_thumbnail_config,
+          
 
         };
 
@@ -563,7 +504,6 @@
             });
 
             setInterval(function () {
-
             }, 900);
 
             /* Progress bar */
@@ -590,60 +530,22 @@
                 }
             });
             
-
             player.on("loadedmetadata", function () {
                 pb.SetDuration(player.duration);
             });
 
             setInterval(function () {
                 pb.SetCurrentProgress(player.currentTime);
-
                 pb.SetBufferProgress(player.duration * player.buffered);
             }, 16);
-            /* Progress bar */
-
-
-            function getStatusObject() {
-                return {
-                    volume: player.volume,
-                    muted: player.muted,
-                    hideControls: player.hideControls,
-                    speed: player.speed,
-                    quality: player.quality,
-                    currentTime: player.currentTime,
-                    playing: player.playing,
-                    paused: player.paused,
-                    stopped: player.stopped,
-                    ended: player.ended,
-                    buffered: player.buffered,
-                    duration: player.duration,
-                    hasAudio: player.hasAudio,
-                    loop: player.loop,
-                    language: player.language,
-                    ratio: player.ratio,
-                };
-            }
+            
         }
-
-        function generateDrmRequest(initDataType, initData, keyContext){
-            return {initDataType:initDataType, initData:initData};
-        }
-            player = new Plyr(video, defaultOptions);
-            initPlayer();
+    
+        player = new Plyr(video, defaultOptions);
+        initPlayer();
         
     });
     
-    function centerVertically() {
-        var container = document.getElementById("body");
-        var inner = document.getElementById("video-container");
-
-        var inHeight = inner.offsetHeight;
-        var conHeight = container.offsetHeight;
-
-        inner.style.marginTop = ((conHeight - inHeight) / 2) + 'px';
-    }
-    centerVertically();
-    window.addEventListener("resize", centerVertically);
   </script>
 
   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
