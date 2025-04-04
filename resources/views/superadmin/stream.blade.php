7f0c3cee60d1d2d64e8af5ad7ae3346264f8a250
asrani <asrani.27@gmail.com>
2025-04-04 14:19:21 +0800

    f

--- resources/views/superadmin/stream.blade.php
+++ resources/views/superadmin/stream.blade.php
@@ -1,56 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>VideoJS</title>
-    <link href="https://vjs.zencdn.net/8.16.1/video-js.css" rel="stylesheet" />
 
-    
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>VideoJS</title>
+  <link href="https://vjs.zencdn.net/8.16.1/video-js.css" rel="stylesheet" />
+
+
   <style>
-    .container{
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        padding: 0px;
+    .container {
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      padding: 0px;
     }
-    .video{
-        padding: 0px;
-        width: 100%;
-        height: 100%;
+
+    .video {
+      padding: 0px;
+      width: 100%;
+      height: 100%;
     }
+
     /* Custom styling for Video.js subtitles */
     .vjs-text-track-display div {
-      color: yellow !important; /* Ubah warna teks subtitle */
+      color: yellow !important;
+      /* Ubah warna teks subtitle */
     }
   </style>
-  
+
 </head>
+
 <body style="margin:0px">
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<div class="container">
-    <video id="my-video" 
-    class="video-js vjs-default-skin video yellow" 
-    controls
-    preload="auto"
-    width="640" 
-    height="360"
-    data-setup="{}">
-            {{-- <source src="{{$public}}" type="application/x-mpegURL" />       --}}
-            <source src="{{$mp4TemporaryUrl}}" type="video/mp4" />
-            {{-- <track kind="captions" src="/srt/deadpool.vtt" srclang="id" label="Indonesia" default> --}}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <div class="container">
+    <video id="my-video" class="video-js vjs-default-skin video yellow" controls preload="auto" width="640" height="360"
+      data-setup="{}">
+      {{--
+      <source src="{{$public}}" type="application/x-mpegURL" /> --}}
+      <source src="{{$mp4TemporaryUrl}}" type="video/mp4" />
+      {{--
+      <track kind="captions" src="/srt/deadpool.vtt" srclang="id" label="Indonesia" default> --}}
     </video>
-</div>
+  </div>
 
-<script src="https://vjs.zencdn.net/8.16.1/video.min.js"></script>
+  <script src="https://vjs.zencdn.net/8.16.1/video.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/7.5.2/videojs.ads.js" integrity="sha512-M/pHAt5s4Eq/RURcqkUTvoyU8EdtL1mQBdzePGQM03shlMWJpBLNHmzaWaYGLZjJhAl0/C6jYFgf8ncNr2802A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-ads/7.5.2/videojs.ads.js"
+    integrity="sha512-M/pHAt5s4Eq/RURcqkUTvoyU8EdtL1mQBdzePGQM03shlMWJpBLNHmzaWaYGLZjJhAl0/C6jYFgf8ncNr2802A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
 
-<script>
+  <script>
     var player = videojs('my-video');
     var adUrl = 'https://www.cpmrevenuegate.com/bq5r5uxdas?key=ad0d6295c26f7e6b4132733881bc9dee';
 
@@ -81,10 +85,10 @@
         location.reload();
       console.log('The video has ended.');
     });
-</script>
+  </script>
 
-<script>
-let textcolor = videojs('my-video');
+  <script>
+    let textcolor = videojs('my-video');
     textcolor.ready(function(){
         var settings = this.textTrackSettings;
         settings.setValues({
@@ -94,8 +98,8 @@ let textcolor = videojs('my-video');
         });
         settings.updateDisplay();
     });
-</script>
-<script>
+  </script>
+  <script>
     function togglePlayPause(){
     var player = videojs('my-video');
         if (player.paused()) {
@@ -120,6 +124,7 @@ $(function() {
     }
   });      
 });
-</script>
+  </script>
 </body>
+
 </html>
--- resources/views/user/stream.blade.php
+++ resources/views/user/stream.blade.php
@@ -404,67 +404,37 @@
       <source src="{{$mp4TemporaryUrl}}" type="video/mp4" />
     </video>
   </div>
-
   <script>
-    // function IsMobile() {
-    //     var check = false;
-    //     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-    //     return check;
-    // }
-    
     document.addEventListener("DOMContentLoaded", async function () {
-        var video = document.querySelector("video");
-        var loadingSpinner = document.getElementById("loading-spinner");
-          
-        // Key untuk localStorage (gunakan hash dari URL video sebagai ID unik)
-        const STORAGE_KEY = 'video-time-' + window.location.href;
-        // Fungsi format waktu (HH:MM:SS)
-        // function formatTime(seconds) {
-        //   return new Date(seconds * 1000).toISOString().substr(11, 8);
-        // }
-
-        var fragmentsLoaded = 0;
-        var videoplayer = null;
-        var isFairplay = false;
-        var useFairplayInternalEngine= false;
-        
-        var isEntDrm = false;
-        var playOriginal = false;
-        var originalUrl = '';
-        var urlPlaylistUrl = '';
-        var isAdPaused = false;
-        var isAirplayEnabled = false;
-
         var video = document.getElementById("main-video");
         var loadingSpinner = document.getElementById("loading-spinner");
-       
+  
+        const STORAGE_KEY = 'video-time-' + window.location.href;
+  
         video.addEventListener("loadeddata", function () {
             loadingSpinner.classList.add("hidden");
         });
-            
+  
         video.addEventListener("click", function () {
             enterFullscreen();
         });
-
-        // Fullscreen + Auto Rotate ke Landscape
+  
         function enterFullscreen() {
             if (video.requestFullscreen) {
                 video.requestFullscreen();
-            } else if (video.webkitRequestFullscreen) { // Safari
+            } else if (video.webkitRequestFullscreen) {
                 video.webkitRequestFullscreen();
-            } else if (video.msRequestFullscreen) { // Internet Explorer
+            } else if (video.msRequestFullscreen) {
                 video.msRequestFullscreen();
             }
-
-            // Kunci ke landscape saat fullscreen
+  
             if (screen.orientation && screen.orientation.lock) {
                 screen.orientation.lock("landscape").catch(function (error) {
                     console.log("Tidak dapat mengunci orientasi: ", error);
                 });
             }
         }
-
-        // Deteksi saat fullscreen aktif
+  
         document.addEventListener("fullscreenchange", function () {
             if (document.fullscreenElement) {
                 console.log("Fullscreen aktif, mengunci orientasi ke landscape...");
@@ -475,7 +445,7 @@
                 }
             }
         });
-
+  
         var defaultOptions = {
             storage: {
                 enabled: true,
@@ -493,15 +463,12 @@
             ],
             settings: ['quality', 'speed', 'loop'],
             speed: { selected: 1, options: [0.5,0.75,1,1.25,1.5,1.75,2,4] },
-          
-
         };
-
+  
         function initPlayer() {
-            // needed for displaying right direction of the captions
             player.elements.captions.dir = "auto";
             $('<div class="plyr__controls__item hide_mobile plyr__spacer"></div>').insertBefore(".plyr__progress__container");
-            /* Heatmap Chart */
+  
             $("video").on('webkitbeginfullscreen webkitendfullscreen', function (event) {
                 if (event.type === 'webkitbeginfullscreen') {
                     document.documentElement.style.setProperty('--webkit-text-track-display', 'block');
@@ -509,23 +476,17 @@
                     document.documentElement.style.setProperty('--webkit-text-track-display', 'none');
                 }
             });
-            
-            /* Progress bar */
-            var scrubbingPaused = false;
-            //var isMobile = IsMobile();
+  
             $(".plyr__progress__container input").css("top", "-5px");
             $(".plyr__progress__container progress").css("top", "4px");
             $(".plyr__progress__container progress").css("opacity", "0.01");
             $(".plyr__progress").prepend($('<div class="plyr__pb"></div>'));
+            
             var pb = new PB(".plyr__pb", ".plyr__progress__container input", {
                 keyColor: "#ff7755",
                 videoLength: 20,
-                chapters: [
-
-                ],
-                moments: [
-
-                ],
+                chapters: [],
+                moments: [],
                 onScrubbingChange: function(seekTime, offset) {
                     var thumbWidth = $(".plyr__preview-thumb").width();
                     var position = Math.max(thumbWidth / 2, offset);
@@ -533,39 +494,48 @@
                     $(".plyr__preview-thumb").css("left", (position - 5.5) + "px");
                 }
             });
-            
+  
             player.on("loadedmetadata", function () {
                 pb.SetDuration(player.duration);
-                 // Lanjutkan dari posisi terakhir jika ada
                 const savedTime = parseFloat(localStorage.getItem(STORAGE_KEY)) || 0;
                 if (savedTime > 0 && savedTime < player.duration) {
                   player.currentTime = savedTime;
                 }
             });
-            // Simpan posisi pemutaran secara berkala
+  
             player.on("timeupdate", function() {
-                  localStorage.setItem(STORAGE_KEY, player.currentTime);
-                });
-
-            // Bersihkan localStorage saat video selesai
+                localStorage.setItem(STORAGE_KEY, player.currentTime);
+            });
+  
             player.on("ended", function() {
               localStorage.removeItem(STORAGE_KEY);
             });
-
+  
             setInterval(function () {
                 pb.SetCurrentProgress(player.currentTime);
                 pb.SetBufferProgress(player.duration * player.buffered);
             }, 16);
-            
         }
-    
+  
         player = new Plyr(video, defaultOptions);
-        
         initPlayer();
         
+        // Toggle play/pause ketika tombol Space ditekan
+        document.addEventListener("keydown", function (e) {
+            if (e.code === "Space" || e.key === " ") {
+                e.preventDefault(); // Hindari scroll jika space ditekan
+                if (player) {
+                    if (player.playing) {
+                        player.pause();
+                    } else {
+                        player.play();
+                    }
+                }
+            }
+        });
     });
-    
   </script>
+
 
 </body>
 
