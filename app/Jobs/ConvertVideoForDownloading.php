09c8006a90f006187cd7f5f98c4928a85847f58d
asrani <asrani.27@gmail.com>
2025-05-15 20:27:26 +0800

    f

--- app/Jobs/ConvertVideoForDownloading.php
+++ app/Jobs/ConvertVideoForDownloading.php
@@ -31,7 +31,7 @@ class ConvertVideoForDownloading implements ShouldQueue
 
         Storage::disk('wasabi')->put('download/' . $this->file->type . '/' . $this->file->filename, $files);
 
-        //Storage::disk('public')->delete($this->file->type . '/' . $this->file->filename);
+        Storage::disk('public')->delete($this->file->type . '/' . $this->file->filename);
         $this->file->update([
             'status_download' => 1,
             'status_stream' => 1
